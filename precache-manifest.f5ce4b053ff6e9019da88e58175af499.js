e1cc34e60188cbf50b6baf4b75f19913f10cc1fc
Shishuai Li <shishuai_li@brown.edu>
2018-11-28 23:17:08 -0500

    Updates

---
+++ precache-manifest.f5ce4b053ff6e9019da88e58175af499.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f58ea842f9e7e2be86e86439fa405011",
+    "url": "/my-app/static/media/img2.f58ea842.jpg"
+  },
+  {
+    "revision": "f8ce347b431a29c86c21",
+    "url": "/my-app/static/css/main.c85f1a94.chunk.css"
+  },
+  {
+    "revision": "e4c2d38a45bce0de076b",
+    "url": "/my-app/static/js/1.e4c2d38a.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1fed1078bbb34e796b45024b2a8ea979",
+    "url": "/my-app/static/media/img10.1fed1078.jpg"
+  },
+  {
+    "revision": "a1e96de025f6e1f6f0a54ce09f8f1ecf",
+    "url": "/my-app/static/media/img12.a1e96de0.jpg"
+  },
+  {
+    "revision": "f8ce347b431a29c86c21",
+    "url": "/my-app/static/js/main.f8ce347b.chunk.js"
+  },
+  {
+    "revision": "5094493f94c3c0ff85425b15e796a7d6",
+    "url": "/my-app/static/media/img3.5094493f.jpg"
+  },
+  {
+    "revision": "f026a24d19eb9f7fc86b4a416c6ef414",
+    "url": "/my-app/static/media/img4.f026a24d.jpg"
+  },
+  {
+    "revision": "ee2e4fb326ac0314b2bc8fdbdecab83e",
+    "url": "/my-app/static/media/img5.ee2e4fb3.jpg"
+  },
+  {
+    "revision": "9b2486589a718aeedc6336c975248131",
+    "url": "/my-app/static/media/img6.9b248658.jpg"
+  },
+  {
+    "revision": "8f9539d53e0afd1f05f28e91894215fb",
+    "url": "/my-app/static/media/img8.8f9539d5.jpg"
+  },
+  {
+    "revision": "a6e9ff1b8b3e29b76db8d70a65427f34",
+    "url": "/my-app/index.html"
+  }
+];
